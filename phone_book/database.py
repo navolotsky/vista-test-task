0ee64a76ae4804e3a66993259c7905d8d0c9cb99
Oleg Navolotsky <oleg.navolotsky@gmail.com>
2022-04-16 01:36:55 +0300

    Improve error processing in database module

--- phone_book/database.py
+++ phone_book/database.py
@@ -1,25 +1,70 @@
 import enum
 import logging
+from typing import Optional, Union
 
-from PyQt5 import QtSql
 from PyQt5.QtCore import Qt
+from PyQt5.QtSql import QSqlDatabase, QSqlError, QSqlQuery, QSqlQueryModel
+from PyQt5.QtWidgets import QMessageBox
 
 logger = logging.getLogger(__name__)
 
 
 class DatabaseError(Exception):
-    pass
+    @staticmethod
+    def from_error(error: QSqlError):
+        err_type_to_exc_cls = {
+            QSqlError.NoError: None,
+            QSqlError.ConnectionError: DatabaseConnectionError,
+            QSqlError.StatementError: DatabaseStatementError,
+            QSqlError.TransactionError: DatabaseTransactionError,
+            QSqlError.UnknownError: DatabaseUnknownError
+        }
+        try:
+            exc_cls = err_type_to_exc_cls[error.type()]
+        except KeyError:
+            raise RuntimeError("unknown value {} of {}".format(error.type(), QSqlError.ErrorType))
+        return None if exc_cls is None else exc_cls(
+            "[{}] {}; {}".format(error.nativeErrorCode(), error.databaseText(), error.driverText()), error)
+
+    def __str__(self):
+        return str(self.args[0]) if self.args else super().__str__()
 
 
 class DatabaseConnectionError(DatabaseError):
     pass
 
 
-def process_query_error(query):
-    exc_cls = DatabaseError
-    if query.lastError().type() == QtSql.QSqlError.ConnectionError:
-        exc_cls = DatabaseConnectionError
-    raise exc_cls(query.lastError().text(), query.lastError())
+class DatabaseStatementError(DatabaseError):
+    pass
+
+
+class DatabaseTransactionError(DatabaseError):
+    pass
+
+
+class DatabaseUnknownError(DatabaseError):
+    pass
+
+
+def process_error(value: Union[QSqlError, QSqlQuery, QSqlDatabase],
+                  raise_exc=True, log=True, show_msg_if_not_conn_err=False) -> Optional[DatabaseError]:
+    error = value.lastError() if isinstance(value, (QSqlQuery, QSqlDatabase)) else value
+    if error.type() == QSqlError.NoError:
+        raise ValueError("there was no error")
+    exc = DatabaseError.from_error(error)
+    if show_msg_if_not_conn_err and not isinstance(exc, DatabaseConnectionError):
+        msg_box = QMessageBox(QMessageBox.Critical, "Телефонная книжка",
+                              "Неожиданная ошибка при работе с базой данных.")
+        msg_box.setDetailedText(str(exc))
+        msg_box.exec()
+    if log:
+        if isinstance(exc, DatabaseConnectionError):
+            logger.warning("Can't connect to database: %s", str(exc))
+        else:
+            logger.error("Got an unexpected database-related error: %s", str(exc))
+    if raise_exc:
+        raise exc
+    return exc
 
 
 def setup_db(settings, default_settings):
@@ -30,7 +75,7 @@ def setup_db(settings, default_settings):
         qsql_driver = settings.value("qsql_driver")
         if qsql_driver is None:
             qsql_driver = default_settings.value("qsql_driver")
-        db = QtSql.QSqlDatabase.addDatabase(qsql_driver, connectionName="main_database")
+        db = QSqlDatabase.addDatabase(qsql_driver, connectionName="main_database")
         if not db.isValid():
             raise RuntimeError("database driver improperly configured")
         for key, method in zip(("host_name", "port", "database_name", "username", "password"),
@@ -47,29 +92,26 @@ def setup_db(settings, default_settings):
 
 
 def get_opened_db():
-    if not QtSql.QSqlDatabase.contains(connectionName="main_database"):
-        raise RuntimeError("You must call `setup_db` at first")
-    db = QtSql.QSqlDatabase.database(connectionName="main_database", open=True)
+    if not QSqlDatabase.contains(connectionName="main_database"):
+        raise RuntimeError("You must call `setup_db()` at first")
+    db = QSqlDatabase.database(connectionName="main_database", open=True)
     if db.isOpenError():
-        logger.error("Cannot open database: %s; Error type: %s", db.lastError().text(), db.lastError().type())
-        if db.lastError().type() == QtSql.QSqlError.ConnectionError:
-            raise DatabaseConnectionError(db.lastError().text(), db.lastError())
-        raise DatabaseError(db.lastError().text(), db.lastError())
+        process_error(db)
     return db
 
 
 def check_session_exists(session_key):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL check_session(:session_key, @session_exists)")
         query.bindValue(":session_key", session_key)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.exec("SELECT @session_exists"):
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
         else:
             return bool(query.value(0))
     finally:
@@ -86,17 +128,17 @@ class RegisterResult(enum.Enum):
 def register(username, email, birth_date):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL register(:username, :email, :birth_date, @result_msg, @password)")
         query.bindValue(":username", username)
         query.bindValue(":email", email)
         query.bindValue(":birth_date", birth_date)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.exec("SELECT @result_msg, @password"):
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
         else:
             return RegisterResult(query.value(0)), query.value(1)
     finally:
@@ -106,16 +148,16 @@ def register(username, email, birth_date):
 def log_in(username_or_email, password):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL log_in(:username_or_email, :password, @session_key)")
         query.bindValue(":username_or_email", username_or_email)
         query.bindValue(":password", password)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.exec("SELECT @session_key"):
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
         else:
             return query.value(0)
     finally:
@@ -125,11 +167,11 @@ def log_in(username_or_email, password):
 def log_out(session_key):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL log_out(:session_key)")
         query.bindValue(":session_key", session_key)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
     finally:
         db.close()
 
@@ -137,15 +179,15 @@ def log_out(session_key):
 def get_user_info(session_key):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL get_user_info(:session_key, @username, @email)")
         query.bindValue(":session_key", session_key)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.exec("SELECT @username, @email"):
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
         else:
             return query.value(0), query.value(1)
     finally:
@@ -155,13 +197,13 @@ def get_user_info(session_key):
 def get_all_contacts(session_key):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL get_all_contacts(:session_key)")
         query.bindValue(":session_key", session_key)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
     finally:
         db.close()
 
@@ -169,15 +211,15 @@ def get_all_contacts(session_key):
 def get_contacts(session_key, letter_set, exclude):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL get_contacts(:session_key, :letter_set, :exclude)")
         query.bindValue(":session_key", session_key)
         query.bindValue(":letter_set", letter_set)
         query.bindValue(":exclude", exclude)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
     finally:
         db.close()
 
@@ -192,18 +234,18 @@ class AddContactResult(enum.Enum):
 def add_contact(session_key, name, phone_number, birth_date):
     db = get_opened_db()
     try:
-        query = QtSql.QSqlQuery(db)
+        query = QSqlQuery(db)
         query.prepare("CALL add_contact(:session_key, :name, :phone_number, :birth_date, @result_msg, @contact_id)")
         query.bindValue(":session_key", session_key)
         query.bindValue(":name", name)
         query.bindValue(":phone_number", phone_number)
         query.bindValue(":birth_date", birth_date)
         if not query.exec():
-            process_query_error(query)
+            process_error(query)
         if not query.exec("SELECT @result_msg, @contact_id"):
-            process_query_error(query)
+            process_error(query)
         if not query.next():
-            process_query_error(query)
+            process_error(query)
         else:
             result_type = AddContactResult(query.value(0))
             contact_id = query.value(1)
@@ -214,20 +256,20 @@ def add_contact(session_key, name, phone_number, birth_date):
         db.close()
 
 
-class ContactsReadWriteModel(QtSql.QSqlQueryModel):
+class ContactsReadWriteModel(QSqlQueryModel):
     def __init__(self, parent, letter_set, exclude=False):
         super().__init__(parent)
         self.letter_set = letter_set
         self.exclude = exclude
 
     def refresh(self, session_key):
-        query = QtSql.QSqlQuery(get_opened_db())
+        query = QSqlQuery(get_opened_db())
         # Have to manually format a string for exec()
         # because using prepare(), bindValue() or addBindValue(), exec()
         # with CALL that should return a SELECT result set doesn't work properly:
         # query.isSelect() returns False and query.size() returns -1
         if not query.exec("CALL get_contacts('{}', '{}', {})".format(session_key, self.letter_set, self.exclude)):
-            process_query_error(query)
+            process_error(query)
         self.setQuery(query)
         for idx, value in enumerate(["Имя", "Телефон", "Дата рождения"], 1):
             self.setHeaderData(idx, Qt.Horizontal, value)
